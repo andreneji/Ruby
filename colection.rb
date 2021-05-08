8dc804303c0972382b7e045c98e32bdb529ba970
andreneji <andreneji@gmail.com>
2021-05-08 01:10:10 -0300

    aula04 - missão01 - incluído colection.rb, cálculo de expoente

---
+++ colection.rb
@@ -0,0 +1,38 @@
+loop do
+  puts 'Programa que calcula o expoente 2'
+  print 'Digite 1 p/ calcular, ou 0 p/ sair: '
+
+  op = gets.chomp.to_i
+
+  if op == 0
+    break
+  elsif op == 1
+
+    puts 'Digite três números'
+
+    array = []
+
+    print 'Digite o primeiro número: '
+    num = gets.chomp.to_i
+    array.push(num)
+
+    print 'Digite o segundo número: '
+    num = gets.chomp.to_i
+    array.push(num)
+
+    print 'Digite o terceiro número: '
+    num = gets.chomp.to_i
+    array.push(num)
+
+    system 'clear'
+
+    for a in array
+      res = "#{a} elevado a 2 = #{a ** 2}"
+      puts res
+    end
+
+  else
+    system 'clear'
+    puts 'Opção Inválida'
+  end
+end
