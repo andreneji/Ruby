60b273f95bce1d315a43ba0c2418ed5004285e9c
andreneji <andreneji@gmail.com>
2021-05-08 01:12:36 -0300

    aula04 - missão01 - alterado colection.rb, inserindo método expoente

--- colection.rb
+++ colection.rb
@@ -1,3 +1,10 @@
+def expoente
+  for a in @array
+    res = "#{a} elevado a 2 = #{a ** 2}"
+    puts res
+  end
+end
+
 loop do
   puts 'Programa que calcula o expoente 2'
   print 'Digite 1 p/ calcular, ou 0 p/ sair: '
@@ -10,27 +17,23 @@ loop do
 
     puts 'Digite três números'
 
-    array = []
+    @array = []
 
     print 'Digite o primeiro número: '
     num = gets.chomp.to_i
-    array.push(num)
+    @array.push(num)
 
     print 'Digite o segundo número: '
     num = gets.chomp.to_i
-    array.push(num)
+    @array.push(num)
 
     print 'Digite o terceiro número: '
     num = gets.chomp.to_i
-    array.push(num)
+    @array.push(num)
 
     system 'clear'
 
-    for a in array
-      res = "#{a} elevado a 2 = #{a ** 2}"
-      puts res
-    end
-
+    expoente
   else
     system 'clear'
     puts 'Opção Inválida'
